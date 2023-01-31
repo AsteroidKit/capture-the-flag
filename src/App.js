dcceb1a0102ce7d3355e6236f318dadf233f980d
niltonce <nilton@chainengine.xyz>
2023-01-31 18:24:09 -0300

    Merge branch 'main' of github.com:AsteroidKit/examples

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import "./App.css";
+import "asteroidkit/styles.css";
+
+import React from "react";
+import { WagmiConfig } from "wagmi";
+import { AsteroidKitProvider, ConnectButton, createClient } from "asteroidkit";
+
+const client = createClient();
+
+const APP_ID = process.env.REACT_APP_ASTEROIDKIT_APPID;
+
+function App() {
+  return (
+    <div className="Appp">
+      <WagmiConfig client={client}>
+        <AsteroidKitProvider appId={APP_ID}>
+          <ConnectButton />
+        </AsteroidKitProvider>
+      </WagmiConfig>
+    </div>
+  );
+}
+
+export default App;
